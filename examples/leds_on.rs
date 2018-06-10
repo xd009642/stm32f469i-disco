1700a926fe5124ef2c8d651c3ae54a421b045c59
xd009642 <danielmckenna93@gmail.com>
2018-06-10 11:52:32 +0100

    Updated for embedded-rust changes
    Also working on example for flashing LEDs.

--- examples/leds_on.rs
+++ examples/leds_on.rs
@@ -1,22 +1,67 @@
 #![no_std]
+#![no_main]
+#![feature(panic_implementation)]
+#![feature(used)]
+#![feature(core_intrinsics)]
 extern crate embedded_hal;
 extern crate stm32f469xx_hal as hal;
+#[macro_use(entry, exception)]
+extern crate cortex_m_rt;
+
+use cortex_m_rt::ExceptionFrame;
 
 use embedded_hal::digital::OutputPin;
+use core::intrinsics;
+use core::panic::PanicInfo;
 use hal::stm32f469xx;
 use hal::gpio::*;
 
-fn main() {
-    let p = stm32f469xx::Peripherals::take().unwrap();
+entry!(main);
+exception!(HardFault, safe_state);
+exception!(*, default_handler);
 
-    let gpiod = p.GPIOD.split();
+fn default_handler(_irqn: i16) {}
 
-    // Turn on PG6 PD4 PD5 PK3
-    
-    // Orange led
-    let mut led2 = gpiod.pd4.into_output_pushpull();
-    led2.set_high();
-    // Red
-    let mut led3 = gpiod.pd5.into_output_pushpull();
-    led3.set_high();
+fn safe_state(_ef: &ExceptionFrame) -> ! {
+    loop {}
 }
+
+fn main() -> ! {
+    let p = stm32f469xx::Peripherals::take().unwrap();
+    let gpiog = p.GPIOG.split();
+    let gpiok = p.GPIOK.split();
+    let gpiod = p.GPIOD.split();
+    let mut led1 = gpiog.pg6.into_output_pushpull().downgrade();
+    let mut led2 = gpiod.pd4.into_output_pushpull().downgrade();
+    let mut led3 = gpiod.pd5.into_output_pushpull().downgrade();
+    let mut led4 = gpiok.pk3.into_output_pushpull().downgrade();
+    
+    // Turn on PG6 PD4 PD5 PK3
+    loop {   
+        for _i in 1..10000 {
+            led1.set_low();
+            // Orange led
+            led2.set_high();
+            // Red
+            led3.set_high();
+            led4.set_low();
+        }
+
+        for _i in 1..10000 {
+            led1.set_high();
+            // Orange led
+            led2.set_low();
+            // Red
+            led3.set_low();
+
+            led4.set_high();
+        }
+    }
+}
+
+#[no_mangle]
+#[panic_implementation]
+pub fn panic(_info: &PanicInfo) -> ! {
+    unsafe { intrinsics::abort() }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 extern crate stm32f469xx_hal as hal;
 extern crate stm32f469xx;
+extern crate embedded_hal;
 
 pub mod leds;
